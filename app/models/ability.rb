cf6554b3ecf069a23fd586e0c3f6cf21cecda3f5
Unnumbered <andrewamigos@gmail.com>
2015-09-07 15:13:11 +0600

    #m-38 fixed work 1h40m

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,48 +3,47 @@ class Ability
 
   def initialize(user)
     if user
-      can [:read, :create], Comment
-      can :read, user
-      can :update, user
+      if user.has_role? :employee
+        common_access user
 
-      if user.has_role? :admin
-        can :manage, :all
+      elsif user.has_role? :mentor
+        common_access user
+
+        can :manage, Meeting, mentor_id: user.id
+        can [:read, :new, :create, :update, :resend], Report, meeting: {mentor_id: user.id}
+
+        can :read, Book, owner_id: user.curator_id
+        can :manage, Album, user_id: user.id
+        can :manage, Photo, user_id: user.id
+
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id}
 
       elsif user.has_role? :curator
-        can :read, User, curator_id: user.id
-        can :read, User.with_role(:curator)
+        common_access user
 
-        can :manage, Child, orphanage_id: user.orphanage_id
         can [:read, :reject, :reject_report, :approve_report], Meeting, mentor: {curator_id: user.id}
         can [:read, :reject, :approve], Report, meeting: { mentor: {curator_id: user.id} }
 
         can :read, Book
         can :manage, Book, owner_id: user.id
-        can :read, Album
         can :manage, Album, user_id: user.id
-        can :read, Photo
         can :manage, Photo, user_id: user.id
 
         can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.subordinates.with_role(:mentor).map(&:id)}
 
-      elsif user.has_role? :mentor
-        can :read, User, id: user.curator_id
-        can :read, Child, orphanage_id: user.orphanage_id
-        can :manage, Meeting, mentor_id: user.id
-        can [:read, :new, :create, :update, :resend], Report, meeting: {mentor_id: user.id}
-
-        can :read, Book, owner_id: user.curator_id
-        can :read, Album
-        can :manage, Album, user_id: user.id
-        can :read, Photo
-        can :manage, Photo, user_id: user.id
-
-        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id}
-
-      elsif user.has_role? :employee
-        can :read, Album
-        can :read, Photo
+      elsif user.has_role? :admin
+        can :manage, :all
       end
     end
   end
+
+  private
+    def common_access user
+      can [:read, :create], Comment
+      can :read, Album
+      can :read, Photo
+      can :read, User, orphanage_id: user.orphanage_id
+      can :update, user
+      can :read, Child, orphanage_id: user.orphanage_id
+    end
 end
