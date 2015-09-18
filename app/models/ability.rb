62e85665e68df46bfb039429dd9e5b8bdd72dfac
Unnumbered <andrewamigos@gmail.com>
2015-09-18 17:53:47 +0600

    #m-47 p5, p7

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,35 +3,59 @@ class Ability
 
   def initialize(user)
     if user
-      if user.has_role? :employee
+      roles = user.roles.map(&:name)
+      collaborators_ids = user.orphanage ? user.orphanage.users.where.not(id: user.id).map(&:id) : []
+
+      if roles.include? 'employee'
         common_access user
 
-      elsif user.has_role? :mentor
+      elsif roles.include? 'mentor'
+        meetings_ids = user.meetings.map(&:id)
+
         common_access user
+        can :read, User, id: user.curator_id
 
         can :manage, Meeting, mentor_id: user.id
-        can [:read, :new, :create, :update, :resend], Report, meeting: {mentor_id: user.id}
+        can [:read, :new, :create, :update, :resend], Report, meeting_id: meetings_ids
 
         can :read, Book, owner_id: user.curator_id
         can :manage, Album, user_id: user.id
         can :manage, Photo, user_id: user.id
 
-        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id,
+                                              trackable_type: 'Meeting', trackable_id: meetings_ids}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id,
+                                              trackable_type: 'Report',
+                                              trackable_id: Report.where(meeting_id: meetings_ids).map(&:id)}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.curator_id,
+                                              trackable_type: 'Book'}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: collaborators_ids,
+                                              trackable_type: 'Photo'}
 
-      elsif user.has_role? :curator
+      elsif roles.include? 'curator'
+        subordinates_ids = user.subordinates.with_role(:mentor).map(&:id)
+
         common_access user
+        can :read, User, curator_id: user.id
 
-        can [:read, :reject, :reject_report, :approve_report], Meeting, mentor: {curator_id: user.id}
-        can [:read, :reject, :approve], Report, meeting: { mentor: {curator_id: user.id} }
+        can [:read, :reject, :reject_report, :approve_report], Meeting, mentor_id: subordinates_ids
+        can [:read, :reject, :approve], Report, meeting: { mentor_id: subordinates_ids }
 
         can :read, Book
         can :manage, Book, owner_id: user.id
         can :manage, Album, user_id: user.id
         can :manage, Photo, user_id: user.id
 
-        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: user.subordinates.with_role(:mentor).map(&:id)}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: subordinates_ids,
+                                              trackable_type: 'Meeting'}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: subordinates_ids,
+                                              trackable_type: 'Report'}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: collaborators_ids,
+                                              trackable_type: 'Book'}
+        can :read, PublicActivity::Activity, {owner_type: 'User', owner_id: collaborators_ids,
+                                              trackable_type: 'Photo'}
 
-      elsif user.has_role? :admin
+      elsif roles.include? 'admin'
         can :manage, :all
       end
     end
